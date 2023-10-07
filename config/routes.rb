f49ebbdc89d7f30cf52ad0d9e85fa312a08cef17
KeiMonden <129693543+kei9625@users.noreply.github.com>
2023-10-07 09:06:41 +0000

    trying to remember how far i did last time

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+# 顧客用
+# URL /customers/sign_in ...
+devise_for :parent, controllers: {
+  registrations: "parents/registrations",
+  sessions: 'parents/sessions'
+}
+
+# 管理者用
+# URL /admin/sign_in ...
+devise_for :employee, controllers: {
+  registrations: "employees/registrations",
+  sessions: "employees/sessions"
+}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
